4f18e9ffd537d173aead90bd293ca96b5da2188e
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2024-07-21 10:40:53 +0200

    first

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="page">Hello Garden 0000—Archive!</div>
+
+	<!-- <NuxtPage></NuxtPage> -->
+</template>
+
+<script setup></script>
+
+<style lang="postcss" scoped>
+	.page {
+		color: white;
+	}
+</style>
